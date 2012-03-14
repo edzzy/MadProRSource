422a8103916182cf6ff03e0a6da5b2498f695172
Edouard Hirchaud <edouard.hirchaud@univ-nantes.fr>
2012-03-14 08:27:15 +0100

    Condition d'utilisation de FC améliorée

--- FC.R
+++ FC.R
@@ -1,12 +1,11 @@
 FC <-function(x){
 #Calcule le fold change entre deux valeurs
 
-	if(length(x) != 2){
+	if(length(x) != 2 | x[1] <=0 | x[2] <= 0 | is.character(x)){
 		stop("Longeur de x doit etre de 2")		
-		return(FALSE)
 	}else{
 
-		if(x[1] > x[2]){
+		if(x[1] >= x[2]){
 			r<- x[1]/x[2]
 		}else{
 			r<-  x[2]/x[1]
