99a9e72d7254c9ab4145d4a57d2bd46356d71784
Edouard Hirchaud <edouard.hirchaud@univ-nantes.fr>
2012-10-16 17:26:56 +0200

    Regroupement de fonctions

--- cluster.R
+++ cluster.R
@@ -1,77 +1,137 @@
-`clusterbyPair` <-
-function(mat,frameFac,nameCl="cluster",order=FALSE){
-	#clustering entre chaque param�tre
-	nc<-0
-	nct<-0
-	nnct<-0
-	for (i in 1:nrow(frameFac)){
-		nl<-nlevels(as.factor(frameFac[i,]))
-		tnl<-(nl*nl -nl) / 2
-		nnct<-nnct+tnl
-		print(nnct)
-			
-	}
-	for (i in 1:nrow(frameFac)){
-		nl<-nlevels(as.factor(frameFac[i,]))
-		if(nl >=3){
-			nlm<-nl-1
-			le<-levels(as.factor(frameFac[i,]))
+detectCluster<-function(pval,seuil=1,seuilCluster=150,pas=200,comparaison_vect=FALSE,mat=FALSE,f=FALSE,out_name="detecClust.png" ){
+	require(genefilter)
+	require(MadProDev)
+	value<- -log10(pval)
+	curveMobile<-rep(1/pas,pas)
+	fil<-filter(value,curveMobile)
+	select<- fil > seuil
+	selectm1<-select[-1]
+#indice de début et de fin, +1 pour le début car le TRUE et sur le "brin" de longeur n-1
+	begin<- which(select == FALSE & selectm1 == TRUE)
+	end<- which(select == TRUE & selectm1 == FALSE)
+
+
+	#sens graphe est de longeur n-1 donne le signe de l'indice n+1 (indice 1 de sens graphe donne le sens de l indice 2 de value
+	sensGraph<-sign(diff(fil))
+	
+	#Preparation à l'extension des pics : Recherche de la prochaine vallé pour le début et la fin
 		
-			for(j in 1:nlm){
-				jp<-j+1
-				for(k in jp:nl){
-					tmpMat<-mat[,which(frameFac[i,]==le[j] | frameFac[i,]==le[k])]
-					tmpFrame<-frameFac[,which(frameFac[i,]==le[j] | frameFac[i,]==le[k])]
-					prefName<-paste(le[j],le[k],sep="-")
-					suffName<-paste(nameCl,"txt",sep=".")
-					nameMat<-paste(prefName,suffName,sep="-")
-					writeMatrice(tmpMat,nameMat)
+	
+	newBegin<-sapply(begin,extendBegin,sensGraph)
+	newEnd<-sapply(end,extendEnd,sensGraph)
 
-					suffName<-paste(nameCl,"frame",sep="-")
-					suffName<-paste(suffName,"txt",sep=".")
-					nameFrame<-paste(prefName,suffName,sep="-")
-					write.table(tmpFrame,nameFrame,col.names=NA,sep="\t",row.names=TRUE);
-					if( nc < 3  &  nct != nnct-1 ){
-						symb="&"
-						ech="1"
-			#			commandCluster<-paste(" cluster -f ",nameCl," -l  -cg m -g 1 -e 1  -m c",sep="")
-			#			system(commandCluster)
-					}else{
-						nc<-0
-						symb=""
-						ech="1"
-			#	commandCluster<-paste(" cluster -f ",nameCl," -l  -cg m -g 1 -e 1  -m c",sep="" & )
-			#	system(commandCluster)		
-						
-					}
-					if(order){
-				#trie des echantillons par classes dans la matrice et dans le tableau de param�tres
-						tmpFrame<-tmpFrame[,names(sort(tmpFrame[i,]))]
-						tmpMat<-tmpMat[,colnames(tmpFrame)]
-				#nom des fichiers matrice et tableau parametres
-						suff<-"order"	
-						prefName<-paste(le[j],le[k],suff,sep="-")
-						suffName<-paste(nameCl,"txt",sep=".")
-						nameMat<-paste(prefName,suffName,sep="-")
-						writeMatrice(tmpMat,nameMat)
+	tailleCluster<-newEnd - newBegin
 
-						suffName<-paste(nameCl,"frame",sep="-")
-						suffName<-paste(suffName,"txt",sep=".")
-						nameFrame<-paste(prefName,suffName,sep="-")
-						write.table(tmpFrame,nameFrame,col.names=NA,sep="\t",row.names=TRUE);
-						ech="0"
-					}	
-					nc<-nc+1			
-					nct<-nct+1			
-					lo<-paste("param",i,"levels",le[j],"level",le[k],"nombre clust en cours",nc ,"nb clust total",nct,"/",nnct,"*",sep=":  ")
-					commandCluster<-paste(" cluster -f ",nameMat," -l  -cg m -g 1 -e ",ech,"  -m c ", symb ,sep="" )
-			
-					print(lo)	
-					print(commandCluster)
-				#	system(commandCluster)		
-				}
-			}
+	selectBegin<-newBegin[which(tailleCluster >= seuilCluster)]
+	selectEnd<-newEnd[which(tailleCluster >= seuilCluster)]
+		 print("TOTO")
+		f<-as.matrix(f)
+		mat<-as.matrix(mat)
+		if (comparaison_vect != FALSE){
+		comparaison_name <-paste(comparaison_vect[1],comparaison_vect[2],sep="-Vs-")
+		m1<-meanByFact(mat,f,comparaison_vect[1])
+		m2<-meanByFact(mat,f,comparaison_vect[2])
+		tabmean<-cbind(m1,m2,pval)
+		value<-apply(tabmean,1,graphClustPval)
 		}
-	}
-}
+			
+		fil<-filter(value,curveMobile)
 
+
+	coordClust<-fusionCluster(selectBegin,selectEnd)
+	jpeg(filename = out_name, width = 1300, height = 900, quality = 100, bg = "white", res = NA)
+	par(mar=c(0,0,0,0),oma=c(0,0,0,0),mgp=c(0,0,0))
+	plot(value,pch=19,cex=0.8,cex.lab=1.5,cex.axis=3,col="slateblue",bty="n",xlab="",ylab="",xaxt="n",yaxt="n",xlim=c(1,length(value)),ylim=range(value,na.rm=TRUE))
+	lines(fil,lwd=5,col="orange2")
+	axis(side=2,tcl=0.5,label=TRUE,pos=c(0,0),cex.axis=3)
+	abline(h=seuil,lwd=2,pch=19,cex=0.8,cex.lab=1.5,cex.axis=1.5,col="darkgreen",lty=2)
+	abline(h=-seuil,lwd=2,pch=19,cex=0.8,cex.lab=1.5,cex.axis=1.5,col="darkgreen",lty=2)
+	abline(h=0,lwd=2,pch=19,cex=0.8,cex.lab=1.5,cex.axis=1.5,col="darkred")
+	abline(v=coordClust[,1],lwd=2,pch=19,cex=0.8,cex.lab=1.5,cex.axis=1.5,col=c(1:nrow(coordClust)))
+	abline(v=coordClust[,2],lwd=2,pch=19,cex=0.8,cex.lab=1.5,cex.axis=1.5,col=c(1:nrow(coordClust)))
+	dev.off()
+
+
+}
+extendBegin<-function(begin,sensGraph){
+	
+	test<-FALSE
+	sens<-sensGraph[begin]
+	while(test == FALSE){
+		indiceValle<-begin -1
+		nbegin<-max(which(sens != sensGraph[1:indiceValle]))
+		if(begin == 51){ 
+			test<-TRUE
+		}else{
+		
+			tmpbegin<-nbegin-50
+			test<- sum(sensGraph[tmpbegin:nbegin]) < -10
+			begin<-nbegin-1
+		}
+		message<-paste(test," ",begin,sep="")
+#		print(message)
+	}
+	nbegin<-nbegin+1
+	return(nbegin)
+
+
+
+}
+extendEnd<-function(end,sensGraph){
+
+	test<-FALSE
+	sens<-sensGraph[end]
+
+
+	while(test == FALSE){
+		indiceDescente<-end+1
+		nend<-min(which(sensGraph[end] != sensGraph[indiceDescente:length(sensGraph)]))	
+		nend<-length(sensGraph[1:end]) +nend
+		tmpend<- nend + 50
+		test<-sum(sensGraph[nend:tmpend]) < -10
+		end<-nend+1
+		}	
+
+	return(nend)
+
+
+}
+fusionCluster<-function(begin,end){
+
+	interXY<-0
+	clusterTotal<-length(begin)
+	finaleBegin<-NULL
+	finaleEnd<-NULL				
+
+	while(length(begin) != 1){		
+		b1<- begin[1]
+		e1<- end[1]
+		b2<- begin[2]
+		e2<- end[2]
+
+		x<-seq(b1:e1) + b1 - 1
+		y<-seq(b2:e2) + b2 - 1
+		interXY<-intersect(x,y)  
+		if(length(interXY) == 0){
+			finaleBegin<-c(finaleBegin,b1)
+			finaleEnd<-c(finaleEnd,e1)
+			begin<-begin[-1]
+			end<-end[-1]
+		}else{
+			rXY<-range(union(x,y))
+			begin<-begin[-1]
+			end<-end[-1]
+			begin[1]<-rXY[1]
+			end[1]<-rXY[2]
+
+		} 
+	}
+
+	finaleBegin<-c(finaleBegin,begin)	
+	finaleEnd<-c(finaleEnd,end)	
+
+	coordClust<-cbind(finaleBegin,finaleEnd)
+	print(finaleBegin)
+	print(finaleEnd)
+	return(coordClust)
+}
--- matrixProcess.R
+++ matrixProcess.R
@@ -1,11 +1,20 @@
-`create_level_matrix` <-
-function(m,classech){
-  val=levels(classech)
-  L=list()
-  for (i in 1:length(val)) {
-    L=c(L,list(m[,which(classech==levels(classech)[i])]))
-  }
-  names(L)= val
-  return (L)
+makeRatio<-function(mat,frameFac,ratio){
+  
+  mat<-mat[,colnames(frameFac)]
+  
+  EchMat<-mat[,which(frameFac[1,] == ratio[1])]
+  RefMat<-mat[,which(frameFac[1,] == ratio[2])] 
+  EchFrame<-frameFac[,which(frameFac[1,] == ratio[1])]
+  RefFrame<-frameFac[,which(frameFac[1,] == ratio[2])]
+  
+  EchFrame<-EchFrame[,order(EchFrame[2,])]
+  RefFrame<-RefFrame[,order(RefFrame[2,])]
+  
+  EchMat<-EchMat[,colnames(EchFrame)]
+  RefMat<-RefMat[,colnames(RefFrame)]
+  
+  matRatio<-EchMat/RefMat
+  namesRatio<-paste(colnames(EchMat),colnames(RefMat),sep="/")
+  colnames(matRatio)<-namesRatio
+  return(matRatio)
 }
-
